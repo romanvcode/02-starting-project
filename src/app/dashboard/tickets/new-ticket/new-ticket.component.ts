67073d03a391763771c1d58e12c6e670c5da0794
romanvcode <roman.v.ua@gmail.com>
2024-06-27 11:29:12 +0300

    Update new-ticket component form handling

--- src/app/dashboard/tickets/new-ticket/new-ticket.component.ts
+++ src/app/dashboard/tickets/new-ticket/new-ticket.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, viewChild } from '@angular/core';
 import { ButtonComponent } from '../../../shared/button/button.component';
 import { ControlComponent } from '../../../shared/control/control.component';
 import { FormsModule } from '@angular/forms';
@@ -11,8 +11,10 @@ import { FormsModule } from '@angular/forms';
   imports: [ButtonComponent, ControlComponent, FormsModule],
 })
 export class NewTicketComponent {
-  onSubmit(title: string, description: string, form: HTMLFormElement) {
+  private form = viewChild.required<ElementRef<HTMLFormElement>>('form');
+
+  onSubmit(title: string, description: string) {
     console.log(title, description);
-    form.reset();
+    this.form().nativeElement.reset();
   }
 }
